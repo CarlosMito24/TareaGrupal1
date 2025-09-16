ef46654a39c85f3878a826e959e00169776bc0eb
abarca43 <josuelinares055@gmail.com>
2025-09-16 16:18:14 -0600

    footer

---
+++ tarea/src/components/FooterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <hr/>
+        <h2>miembros del grupo</h2>
+        <ol><li>Carlos Alfonso Mito De León</li>
+        <li>Melissa Nicole Elena Fuentes</li>
+        <li>Kevin Santiago Mate Perez</li>
+        <li>Edgardo Israel Tobar Claros</li>
+        <li>Josue David Abarca Linares</li></ol>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
